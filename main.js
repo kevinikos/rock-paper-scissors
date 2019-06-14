f90acd3da534abb66b3aac39163cfde546be9f71
kevinikos <kevin.nikolas123@gmail.com>
2019-06-14 15:25:14 +0200

    Code rebuild

--- main.js
+++ main.js
@@ -1,162 +1,87 @@
+const gameResults = {
+  playerHand: "",
+  computerHand: "",
+  winner: ""
+};
+
+const gameSummary = {
+  numberOfGames: 0,
+  wins: 0,
+  losses: 0,
+  draws: 0
+};
+
+const userName = prompt("Choose your name:");
+const availableMoves = [...document.querySelectorAll("div[class*='img']")];
 const startButton = document.querySelector("button");
-const rock = document.querySelector(".rock");
-const paper = document.querySelector(".paper");
-const scissors = document.querySelector(".scissors");
-const userChoice = document.getElementById("user");
-const computerChoice = document.getElementById("computer");
-const winner = document.getElementById("winner");
-const numberOfGames = document.getElementById("games");
-const numberOfWins = document.getElementById("wins");
-const numberOfLosses = document.getElementById("losses");
-const numberOfDraws = document.getElementById("draws");
-const options = ["rock", "paper", "scissors"];
-let choiceStatus = true;
-let rockStatus = false;
-let paperStatus = false;
-let scissorsStatus = false;
-let games = 0;
-let wins = 0;
-let losses = 0;
-let draws = 0;
-const userName = prompt("Type your nickname:");
-userChoice.textContent = `Your choice:`;
-computerChoice.textContent = "Computer choice:";
-winner.textContent = "Winner:";
-numberOfGames.textContent = `Number of games: ${games}`;
-numberOfWins.textContent = `Wins: ${wins}`;
-numberOfLosses.textContent = `Losses: ${losses}`;
-numberOfDraws.textContent = `Draws: ${draws}`;
 
-const randomChoice = () => {
-  const index = Math.floor(Math.random() * options.length);
-  return options[index];
-};
+function randomSelection() {
+  const index = Math.floor(Math.random() * availableMoves.length);
+  return availableMoves[index].dataset.option;
+}
 
-const gameplay = () => {
-  const computer = randomChoice();
-  console.log(`computer choice: ${computer}`);
-  switch (choiceStatus) {
-    case rockStatus:
-      switch (computer) {
-        case "rock":
-          userChoice.textContent = "Your choice: rock";
-          computerChoice.textContent = `Computer choice: ${computer}`;
-          winner.textContent = "Winner: ---";
-          draws++;
-          break;
-        case "paper":
-          userChoice.textContent = "Your choice: rock";
-          computerChoice.textContent = `Computer choice: ${computer}`;
-          winner.innerHTML = "Winner: <strong>Computer</strong>";
-          losses++;
-          break;
-        case "scissors":
-          userChoice.textContent = "Your choice: rock";
-          computerChoice.textContent = `Computer choice: ${computer}`;
-          winner.innerHTML = `Winner: <strong>${userName}</strong>`;
-          wins++;
-          break;
-      }
-      rockStatus = !rockStatus;
-      rock.classList.toggle("square__border");
-      games++;
-      break;
-    case paperStatus:
-      switch (computer) {
-        case "rock":
-          userChoice.textContent = "Your choice: paper";
-          computerChoice.textContent = `Computer choice: ${computer}`;
-          winner.innerHTML = `Winner: <strong>${userName}</strong>`;
-          wins++;
-          break;
-        case "paper":
-          userChoice.textContent = "Your choice: paper";
-          computerChoice.textContent = `Computer choice: ${computer}`;
-          winner.textContent = "Winner: ---";
-          draws++;
-          break;
-        case "scissors":
-          userChoice.textContent = "Your choice: paper";
-          computerChoice.textContent = `Computer choice: ${computer}`;
-          winner.innerHTML = `Winner: <strong>Computer</strong>`;
-          losses++;
-          break;
-      }
-      paperStatus = !paperStatus;
-      paper.classList.toggle("square__border");
-      games++;
-      break;
-    case scissorsStatus:
-      switch (computer) {
-        case "rock":
-          userChoice.textContent = "Your choice: scissors";
-          computerChoice.textContent = `Computer choice: ${computer}`;
-          winner.innerHTML = `Winner: <strong>Computer</strong>`;
-          losses++;
-          break;
-        case "paper":
-          userChoice.textContent = "Your choice: scissors";
-          computerChoice.textContent = `Computer choice: ${computer}`;
-          winner.innerHTML = `Winner: <strong>${userName}</strong>`;
-          wins++;
-          break;
-        case "scissors":
-          userChoice.textContent = "Your choice: scissors";
-          computerChoice.textContent = `Computer choice: ${computer}`;
-          winner.textContent = "Winner: ---";
-          draws++;
-          break;
-      }
-      scissorsStatus = !scissorsStatus;
-      scissors.classList.toggle("square__border");
-      games++;
-      break;
-    default:
-      userChoice.textContent = `Your choice:`;
-      computerChoice.textContent = "Computer choice:";
-      winner.textContent = "Winner:";
-      alert("Pick your weapon");
+function handSelection() {
+  availableMoves.forEach(hand => {
+    hand.style.boxShadow = "";
+    this.style.boxShadow = "0 0 0 4px yellow";
+  });
+  gameResults.playerHand = this.dataset.option;
+  gameResults.computerHand = randomSelection();
+}
+
+function checkResult() {
+  const player = gameResults.playerHand;
+  const computer = gameResults.computerHand;
+  gameSummary.numberOfGames++;
+  if (player === computer) {
+    gameResults.winner = "---";
+    gameSummary.draws++;
+  } else if (
+    (player === "rock" && computer === "scissors") ||
+    (player === "paper" && computer === "rock") ||
+    (player === "scissors" && computer === "paper")
+  ) {
+    gameResults.winner = userName;
+    gameSummary.wins++;
+  } else {
+    gameResults.winner = "Computer";
+    gameSummary.losses++;
   }
-  numberOfGames.textContent = `Number of games: ${games}`;
-  numberOfWins.textContent = `Wins: ${wins}`;
-  numberOfLosses.textContent = `Losses: ${losses}`;
-  numberOfDraws.textContent = `Draws: ${draws}`;
-};
+}
 
-rock.addEventListener("click", () => {
-  if (paperStatus) {
-    paperStatus = !paperStatus;
-    paper.classList.toggle("square__border");
-  } else if (scissorsStatus) {
-    scissorsStatus = !scissorsStatus;
-    scissors.classList.toggle("square__border");
+function showResults() {
+  //left-panel
+  document.getElementById("user").textContent = gameResults.playerHand;
+  document.getElementById("computer").textContent = gameResults.computerHand;
+  document.getElementById("winner").innerHTML = `<strong>${
+    gameResults.winner
+  }</strong>`;
+  //right-panel
+  document.getElementById("games").textContent = gameSummary.numberOfGames;
+  document.getElementById("wins").textContent = gameSummary.wins;
+  document.getElementById("losses").textContent = gameSummary.losses;
+  document.getElementById("draws").textContent = gameSummary.draws;
+}
+
+function resetGame() {
+  document.querySelector(`.img__${gameResults.playerHand}`).style.boxShadow =
+    "";
+  gameResults.playerHand = "";
+  gameResults.computerHand = "";
+  gameResults.winner = "";
+}
+
+function startGame() {
+  if (!gameResults.playerHand) {
+    return alert("Choose your hand ");
   }
-  rockStatus = !rockStatus;
-  rock.classList.toggle("square__border");
+  checkResult();
+  showResults();
+  resetGame();
+}
+
+availableMoves.forEach(hand => {
+  hand.addEventListener("click", handSelection);
 });
 
-paper.addEventListener("click", () => {
-  if (rockStatus) {
-    rockStatus = !rockStatus;
-    rock.classList.toggle("square__border");
-  } else if (scissorsStatus) {
-    scissorsStatus = !scissorsStatus;
-    scissors.classList.toggle("square__border");
-  }
-  paperStatus = !paperStatus;
-  paper.classList.toggle("square__border");
-});
-
-scissors.addEventListener("click", () => {
-  if (rockStatus) {
-    rockStatus = !rockStatus;
-    rock.classList.toggle("square__border");
-  } else if (paperStatus) {
-    paperStatus = !paperStatus;
-    paper.classList.toggle("square__border");
-  }
-  scissorsStatus = !scissorsStatus;
-  scissors.classList.toggle("square__border");
-});
-
-startButton.addEventListener("click", gameplay);
+startButton.addEventListener("click", startGame);
